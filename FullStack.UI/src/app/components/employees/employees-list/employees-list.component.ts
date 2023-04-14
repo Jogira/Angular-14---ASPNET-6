4c0881ee37434f60fc70b996f5df2227b979dd63
Jonathan <Zweitonegoismus@gmail.com>
2023-04-14 13:31:57 -0400

    Employees table and class created.

--- FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./employees-list.component.css']
 })
 export class EmployeesListComponent {
+  employees: Employee[] = [
+    {
+      id: '5abvcaaca-asdasd-qwqea-z113-aasf',
+      name: 'Jack-o',
+      email: 'Valentine.aria@email.com',
+      phone: 1233214567,
+      salary: 50000,
+      department: 'Gear Research'
+    },
+    {
+      id: 'dasd13-asdasd-qwqea-z113-asd1x',
+      name: 'May',
+      email: 'Tostugeki@email.com',
+      phone: 999999182,
+      salary: 42000,
+      department: 'Sea World'
+    },
+    {
+      id: 'aszc-asdasd-xzczxc-z113-a134g',
+      name: 'Ramlethal',
+      email: 'Rekka.aria@email.com',
+      phone: 1233214567,
+      salary: 50000,
+      department: 'Gear Research'
+    }
+  ];
 
+  ngOnInit(): void {
+    // this.employees.push()
+  }
 }
