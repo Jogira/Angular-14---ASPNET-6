f0dd904ae17aa2b911d9b2146c5ee8e2bd43a54f
Jonathan <Zweitonegoismus@gmail.com>
2023-04-25 13:35:29 -0400

    Edit employee page and view button added to table.

--- FullStack.UI/src/app/components/employees/edit-employee/edit-employee.component.ts
+++ FullStack.UI/src/app/components/employees/edit-employee/edit-employee.component.ts
@@ -1,4 +1,7 @@
+import { EmployeesService } from 'src/app/services/employees.service';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-edit-employee',
@@ -7,4 +10,35 @@ import { Component } from '@angular/core';
 })
 export class EditEmployeeComponent {
 
+  employeeDetails: Employee =
+    {
+      id: '',
+      name: '',
+      email: '',
+      phone: 0,
+      salary: 0,
+      department: ''
+    };
+  constructor(private route: ActivatedRoute, private employeeService: EmployeesService) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      {
+        next: (params) => {
+          const id = params.get('id');
+
+          if (id) {
+            console.log("CAllING API")
+            //Call API
+            this.employeeService.getEmployee(id)
+              .subscribe({
+                next: (response) => {
+                  this.employeeDetails = response;
+                }
+              });
+          }
+        }
+      }
+    )
+  }
 }
